b733efb77b9dd22f2cc1df89e12ec378a5c01662
RashmiMuraharishetti <rashmi.muraharishetti@gmail.com>
2018-12-13 13:30:06 -0700

    First Commit

---
+++ src/main/java/com/assessment/kumquatRepublic/KumquatRepublicApplication.java
@@ -0,0 +1,14 @@
+package com.assessment.kumquatRepublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumquatRepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumquatRepublicApplication.class, args);
+	}
+
+}
+
